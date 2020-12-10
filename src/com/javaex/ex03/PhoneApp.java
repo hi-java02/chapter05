3c9fab39b23be0d77698ac32120d9bb3833136e3
iremys <iremys@gmail.com>
2020-12-10 15:48:17 +0900

    I/O스트림

---
+++ src/com/javaex/ex03/PhoneApp.java
@@ -0,0 +1,12 @@
+package com.javaex.ex03;
+
+public class PhoneApp {
+
+	public static void main(String[] args) {
+		//리스트 X
+		
+		
+
+	}
+
+}
---
+++ src/com/javaex/ex04/Person.java
@@ -0,0 +1,12 @@
+package com.javaex.ex04;
+
+public class Person {
+	
+	//필드
+	private String nmae;
+	private String hp;
+	private String company;
+
+	
+	
+}
